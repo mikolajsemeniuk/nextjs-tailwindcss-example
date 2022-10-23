50c40e9679c8381cbc190767390060bef6ef60b0
mikolaj semeniuk <m.semeniuk@icloud.com>
2022-10-23 15:00:13 +0200

    first commit

---
+++ pages/index.tsx
@@ -0,0 +1,13 @@
+import type { NextPage } from "next";
+
+const Home: NextPage = () => {
+  return (
+    <>
+      <h1 className="text-3xl text-yellow-300 font-bold underline">
+        Hello world!
+      </h1>
+    </>
+  );
+};
+
+export default Home;
